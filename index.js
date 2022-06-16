14556a7370c06fcb7d35c56ac6789854433bb82e
cotzhang <3063925756@qq.com>
2022-06-16 14:16:53 +0800

    Completed first version

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const glasstron = require('glasstron');
 const electron = require('electron');
+const remote = require("@electron/remote/main")
+remote.initialize();
 let win;
 electron.app.commandLine.appendSwitch("enable-transparent-visuals");
 electron.app.on('ready', () => {
@@ -16,20 +18,21 @@ electron.app.on('ready', () => {
 
 function spawnWindow(){
 	win = new glasstron.BrowserWindow({
-		width: 800,
-		height: 600,
-		frame: false
+		width: 600,
+		height: 400,
+		frame: false,
+		resizable:false,
+		webPreferences: {
+	  		nodeIntegration: true, 
+	  		contextIsolation: false
+    	}
 	});
 	win.blurType = "blurbehind";
 	win.setBlur(true);
-	win.loadFile('index.html')
+	win.loadFile('app/index.html')
 	win.removeMenu() 
+	//win.setAlwaysOnTop("alwaysOnTop")
 	win.webContents.openDevTools({mode:"detach"})
+	remote.enable(win.webContents)
 	return win;
 }
-
-setTimeout(function() {
-	win.transparent=false;
-	console.log(win);
-},2000)
-// ...
